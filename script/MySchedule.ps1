9d987d1acd9a0dea8bc4dc3fbd610ecb81d4d11d
karlronsaria <andrew.mikedaniels@gmail.com>
2022-12-24 16:33:02 -0800

    feat: script/MySchedule.ps1

---
+++ script/MySchedule.ps1
@@ -0,0 +1,280 @@
+# # todo
+# - [ ] test
+function Find-MyTree {
+    Param(
+        [ArgumentCompleter({
+            $default = cat $PsScriptRoot\..\res\default.json | ConvertFrom-Json
+            $path = $default.SearchDirectory
+            return (dir $path -Directory).Name
+        })]
+        [String]
+        $Subdirectory,
+
+        [String[]]
+        $Tag,
+
+        [ValidateSet('Open', 'Cat', 'Tree')]
+        [String]
+        $Mode = 'Tree',
+
+        [String]
+        $Directory = $( `
+            (cat $PsScriptRoot\..\res\default.json `
+                | ConvertFrom-Json).SearchDirectory `
+        ),
+
+        [Int]
+        $DepthLimit = 4
+    )
+
+    $settings = cat $PsScriptRoot\..\res\default.json | ConvertFrom-Json
+    $FoldOnProperties = $settings.FoldSubtreeOnProperties
+    $path = Join-Path (Join-Path $Directory $Subdirectory) '*.md'
+
+    $tree =
+        $path `
+        | Get-ChildItem `
+            -Recurse `
+        | Get-Content `
+        | Get-MarkdownTable `
+            -DepthLimit $DepthLimit
+
+    if ($Tags.Count -gt 0) {
+        $tree =
+            $tree `
+            | Find-Subtree `
+                -PropertyName tag `
+            | where {
+                $_.tag.ToLower() -in $Tag.ToLower()
+            }
+    }
+
+    return $tree `
+        | Get-FoldedSubtree `
+            -FoldOnProperty $FoldOnProperties `
+        | Write-MarkdownTree
+}
+
+# # todo
+# - [ ] test
+function Get-MySchedule {
+    Param(
+        [ArgumentCompleter({
+            $default = cat $PsScriptRoot\..\res\default.json | ConvertFrom-Json
+            $path = $default.ScheduleDirectory
+            return (dir $path -Directory).Name
+        })]
+        [String]
+        $Subdirectory,
+
+        [ValidateSet('Schedule', 'Open', 'Cat', 'Tree')]
+        [String]
+        $Mode = 'Schedule',
+
+        [String[]]
+        $Pattern,
+
+        [String]
+        $StartDate,
+
+        [String]
+        $Directory = $( `
+            (cat $PsScriptRoot\..\res\default.json `
+                | ConvertFrom-Json).ScheduleDirectory `
+        ),
+
+        [String]
+        $Extension = $( `
+            (cat $PsScriptRoot\..\res\default.json `
+                | ConvertFrom-Json).ScheduleExtension `
+        ),
+
+        # todo
+        # - [ ] WhatIf
+    )
+
+    function Get-NewDirectoryPath {
+        Param(
+            [String]
+            $Directory,
+
+            [String]
+            $Subdirectory,
+
+            [String]
+            $DefaultSubdirectory,
+
+            [String]
+            $Extension
+        )
+
+        $path = if ($Subdirectory) {
+            Join-Path $Directory $Subdirectory
+        }
+        elseif ($DefaultSubdirectory) {
+            Join-Path $Directory $DefaultSubdirectory
+        }
+        else {
+            $Directory
+        }
+
+        if ($Extension) {
+            $path = Join-Path $path $Extension
+        }
+
+        return $path
+    }
+
+    function Get-ScheduleObject {
+        Param(
+            [Object[]]
+            $File,
+
+            [Object[]]
+            $JsonFile,
+
+            [String]
+            $StartDate,
+
+            [PsCustomObject]
+            $Default,
+
+            [String]
+            $DefaultsFileName
+        )
+
+        $schedule =
+            Get-ChildItem `
+                -Path $File `
+                -Recurse `
+            | Get-Content `
+            | Get-Schedule `
+                -StartDate:$StartDate `
+                -Default:$Default
+
+        if ((Test-Path $JsonFile)) {
+            $subtables =
+                Get-ChildItem `
+                    -Path $JsonFile `
+                    -Recurse `
+                | where {
+                    $DefaultsFileName -ne $_.Name.ToLower()
+                } | foreach {
+                    cat $_ | ConvertFrom-Json
+                }
+
+            $schedule = $schedule `
+                | Add-Schedule `
+                    -Table $subtables `
+                    -StartDate:$StartDate
+        }
+
+        return $schedule
+    }
+
+    $getScripts =
+        "$env:OneDrive\Documents\devlib\powershell\PsSchedule\Get-Scripts.ps1"
+
+    & $getScripts | % { . $_ }
+
+    $settings = cat $PsScriptRoot\..\res\default.json | ConvertFrom-Json
+    $DefaultsFileName = $settings.ScheduleDefaultsFile
+    $OpenCommand = $settings.OpenCommand
+    $FoldOnProperties = $settings.FoldSubtreeOnProperties
+    $DefaultSubdirectory = $settings.ScheduleDefaultSubdirectory
+
+    $DefaultSubdirectory = switch ($Mode) {
+        'Cat' { '' }
+        'Open' { '' }
+        'Tree' { '' }
+        'Style' { $DefaultSubdirectory }
+    }
+
+    $path = Get-NewDirectoryPath `
+        -Directory $Directory `
+        -Subdirectory $Subdirectory `
+        -DefaultSubdirectory $DefaultSubdirectory
+
+    if (-not (Test-Path $path)) {
+        return @()
+    }
+
+    $files = Join-Path $path $Extension
+    $defaultsPath = Join-Path $path $DefaultsFileName
+
+    $defaults = if ((Test-Path $defaultsPath)) {
+        cat $defaultsPath | ConvertFrom-Json
+    } else {
+        $null
+    }
+
+    $jsonFiles = Join-Path $path "*.json"
+
+    if ($Pattern.Count -gt 0) {
+        $files = $Pattern | foreach {
+            dir $files `
+                -Recurse `
+            | Select-String $_ `
+            | sort -Property Path -Unique
+        }
+
+        $jsonFiles = $Pattern | foreach {
+            dir $jsonFiles `
+                -Recurse `
+            | Select-String $_ `
+            | sort -Property Path -Unique
+        }
+
+        if ($Mode -eq 'Open') {
+            foreach ($sls in (@($files) + @($jsonFiles))) {
+                start "$OpenCommand" `
+                    -ArgumentList "$($sls.Path)", "+$($sls.LineNumber)"
+            }
+
+            Write-Output $files
+            Write-Output $jsonFiles
+            return
+        }
+
+        $files = $files.Path
+        $jsonFiles = $jsonFiles.Path
+    }
+
+    if ($Mode -eq 'Open') {
+        foreach ($sls in (@($files) + @($jsonFiles))) {
+            start "$OpenCommand" `
+                -ArgumentList "$($sls.Path)"
+        }
+
+        Write-Output $files
+        Write-Output $jsonFiles
+        return
+    }
+
+    if ($Mode -eq 'Cat') {
+        $files | cat
+        $jsonFiles | cat
+        return
+    }
+
+    $schedule = Get-ScheduleObject `
+        -File:$files `
+        -JsonFile:$jsonFiles `
+        -StartDate:$StartDate `
+        -Default:$defaults
+        -DefaultsFileName:$DefaultsFileName
+
+    switch ($Mode) {
+        'Schedule' {
+            return $schedule | Write-Schedule
+        }
+
+        'Tree' {
+            return $schedule `
+                | Get-FoldedSubtree `
+                    -FoldOnProperty $FoldOnProperties `
+                | Write-MarkdownTree
+        }
+    }
+}
+
